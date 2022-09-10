5ecaca2fa4e59888abb85f226fa9a42360a9273c
samuel.jansen <samuel.jansenn@gmail.com>
2022-09-10 01:53:51 -0300

    feature: config

---
+++ src/SetupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/admin',
+    createProxyMiddleware({
+      target: 'http://localhost:7888', 
+      changeOrigin: true,
+    })
+  );
+};
